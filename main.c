5c74188f30ddd3fa83908df4eb851889834a9896
Zhao Chun <buaa.zhaoc@gmail.com>
2011-12-23 20:19:47 +0800

    add main.c

---
+++ main.c
@@ -0,0 +1,35 @@
+/**
+ * @file main.c
+ * @author imay
+ * @date 2011/12/23 20:18:36
+ * @version $Revision$ 
+ * @brief 
+ *  
+ **/
+
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+    printf("hello github.\n");
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* vim: set ts=4 sw=4 sts=4 tw=100 noet: */
